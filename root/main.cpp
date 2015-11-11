fac241d77f327196b2abb21cc0b8c2540ff426a3
marcoareis <marcoreis.aa@gmail.com>
2015-11-11 17:40:18 -0300

    sonar plot changes hohoho

--- root/main.cpp
+++ root/main.cpp
@@ -6,232 +6,309 @@
 int main(int argc, char **argv) {
 
 	cv::Mat3b image = cv::Mat3b::zeros(600, 1000);
-	cv::Point center(image.cols / 2, image.rows * 0.9);
+	cv::Point center(image.cols * 0.5, image.rows * 0.9);
 
-	double ratio = M_PI / 180;
-	double offset = -M_PI_2;
-	double start_angle = -60.0 * ratio + offset;
-	double end_angle = 60.0 * ratio + offset;
-	int bins = 500;
-	int beams = 256;
+	// fixed variables (parametrizavel)
+	int beams = 256, bins = 500;
+	double fovx = 120.0, start_angle = -60.0;
 
-	double radiusLimit = image.cols/ 2.0;
-	double radiusIncrement = radiusLimit / bins;
-
-	double angleLimit = 210.0 * ratio + offset;
-	double angleIncrement = angleLimit / beams;
-
-	std::vector<int> pixelCartesianPositions;
+	// so...
+	double rad_beg = start_angle * M_PI / 180;
+	double rad_end = (start_angle + fovx) * M_PI / 180;
+	double beam_size = (fovx * M_PI / 180) / beams;
 
 	std::vector<int> transfer;
 
 	//  delimiting each sector
-
-	int my_bin, my_beam;
-
-	my_beam = 256 - my_beam;
-	double minBin, maxBin;
-	minBin = (my_bin - 1) * radiusIncrement;
-	maxBin = my_bin * radiusIncrement;
-
-	double minAngle, maxAngle;
-	minAngle =  (my_beam - 1) * (angleIncrement) - (5*M_PI/6);
-	maxAngle =  my_beam * (angleIncrement)  - (5*M_PI/6);
-
-
-	//				std::cout << "this is the min angle ---->" << minAngle / ratio << std::endl;
-	//				std::cout << "this is the max angle ----------->" << maxAngle / ratio << std::endl;
-	//				std::cout << "this is the angle increment ----------->" << angleIncrement / ratio << std::endl;
-
-
-	//      Here starts the sweep
-
 	for (int imgx = 0; imgx < image.cols; ++imgx) {
 		for (int imgy = 0; imgy < center.y; ++imgy) {
 
-
-			std::vector<int> beams_identification;
-			std::vector<int> bins_identification;
-
 			cv::Point2f point(imgx - center.x, imgy - center.y);
-			double radius = sqrt(point.x * point.x + point.y * point.y);
-			double current_angle;
 
-			// Selecting the current angle to the next step
-
-			if (point.y != 0) {
-				current_angle = atan((point.x) / (point.y));
-			} else
-				current_angle = M_PI_2;
-
-			current_angle += offset;
-
-
-			//				if(current_angle == -M_PI / 2){
-			//						// isso aqui eh so pra saber onde eu estou, tipo uma bussola
-			//					image[imgy][imgx][0] = 255;
-			//					image[imgy][imgx][1] = 255;
-			//					image[imgy][imgx][2] = 0;
-			//				}
-
-
-
-
-
-			if (radius >= radiusLimit || start_angle >= current_angle || current_angle >= end_angle){
-				//aqui eu estou fora do grid
+			if (point.y == 0) {
 				transfer.push_back(-1);
+			} else {
+				double theta = atan(point.x / point.y);
+				double radius = sqrt(point.x * point.x + point.y * point.y);
 
-			}
+				// pixels outside of sonar image
+				if (radius >= bins || rad_beg > theta || theta > rad_end || !radius)
+					transfer.push_back(-1);
 
-
-
-			else {
-				//aqui eu estou dentro do grid, so estou trabalhando com os pixels que irei armazenar
-
-				int start_beam;
-
-
-				if (current_angle >= - M_PI/2) {
-
-					for (int my_beam = 255; my_beam > beams/2; my_beam--)	{		//Primeiro pedreiro
-
-
-						image[imgy][imgx][0] = 0;
-						image[imgy][imgx][1] = 0;
-						image[imgy][imgx][2] = 255;
-
-
-						int my_bin, id_bins, id_beam;
-
-						my_beam = 256 - my_beam;
-						double minBin, maxBin;
-						minBin = (my_bin - 1) * radiusIncrement;
-						maxBin = my_bin * radiusIncrement;
-
-						double minAngle, maxAngle;
-						minAngle =  (my_beam - 1) * (angleIncrement) - (5*M_PI/6);
-						maxAngle =  my_beam * (angleIncrement)  - (5*M_PI/6);
-
-
-
-						if (current_angle >= minAngle && current_angle < maxAngle){
-
-							std::cout << "PLEEEEI" << std::endl;
-
-							beams_identification.push_back(my_beam);
-							bins_identification.push_back(radius);
-
-						}
-
-						//						std::cout << "this is the min angle ---->" << minAngle / ratio << std::endl;
-						//						std::cout << "this is the max angle ----------->" << maxAngle / ratio << std::endl;
-						//						std::cout << "my_beam -->" << my_beam << std::endl;
-
-						//						std::cout << "bins_identification -->" << bins_identification.at(my_beam) << std::endl;
-						//						std::cout << "Beams_identification -->" << beams_identification.at(my_beam) << std::endl;
-
-						break;
-					}
-				}
-
-
+				// pixels inside of sonar image
 				else {
 
+					int id_beam;
 
+					for (int id_beam = 0; id_beam < beams - 1; ++id_beam) {
+						id_beam * beam_size
 
-					for (int my_beam = beams/2; my_beam > 0; my_beam--){	//Segundo pedreiro
-
-						image[imgy][imgx][0] = 0;
-						image[imgy][imgx][1] = 255;
-						image[imgy][imgx][2] = 0;
-
-
-						int my_bin, id_bins, id_beam;
-
-						my_beam = 256 - my_beam;
-						double minBin, maxBin;
-						minBin = (my_bin - 1) * radiusIncrement;
-						maxBin = my_bin * radiusIncrement;
-
-						double minAngle, maxAngle;
-						minAngle =  (my_beam - 1) * (angleIncrement) - (5*M_PI/6);
-						maxAngle =  my_beam * (angleIncrement)  - (5*M_PI/6);
-
-						if (current_angle >= minAngle && current_angle < maxAngle){
-
-//							std::cout << "PLEEEEI" << std::endl;
-
-							beams_identification.push_back(my_beam);
-							bins_identification.push_back(radius);
-
-						}
-
-//						std::cout << "this is the min angle ---->" << minAngle / ratio << std::endl;
-//						std::cout << "this is the max angle ----------->" << maxAngle / ratio << std::endl;
-//						std::cout << "my_beam -->" << my_beam << std::endl;
-
-						//						std::cout << "bins_identification -->" << bins_identification.at(my_beam) << std::endl;
-						//						std::cout << "Beams_identification -->" << beams_identification.at(my_beam) << std::endl;
-
-						break;
 
 					}
 
+//					transfer.push_back(0);
 
 				}
-
-
 			}
+		}
+	}
 
 
+	std::cout << "Image: " << center.y * image.cols << std::endl;
+	std::cout << "Transfer: " << transfer.size() << std::endl;
 
-			//			current_angle <= 0 ? start_beam = 0 : start_beam = beams/2;
-
-
-
-
-
-
-			// 23859176587921364578936248975623897464896598461986238461892634891// 23859176587921364578936248975623897464896598461986238461892634891
-
-
-			//			if (radius < radiusLimit && start_angle < current_angle && current_angle < end_angle) {
-			//
-			//				image[imgy][imgx][0] = 255;
-			//				image[imgy][imgx][1] = 0;
-			//				image[imgy][imgx][2] = 0;
-
-			//&& (int) radius == my_bin
-
-			//				if ( (current_angle > minAngle && current_angle < maxAngle )){
-			//
-			//					//paint in yellow
-			//
-			//					image[imgy][imgx][0] = 0;
-			//					image[imgy][imgx][1] = 255;
-			//					image[imgy][imgx][2] = 255;
-
-			//				std::cout << "These are the point(s) coordinates -->" << cv::Point(imgx, imgy) << std::endl;
-			//				}
-
-
-			//				transfer.push_back(imgy * image.cols + imgx);
-
-
-			//	}
-
-			//		pixelCartesianPositions.push_back(imgy * image.cols + imgx);
-
-
-			// Here ends the sweep
+	// plot sonar image
+	for (int x = 0; x < image.cols; ++x) {
+		for (int y = 0; y < center.y; ++y) {
+			if (!transfer[x * center.y + y]) {
+				image[y][x][0] = 255;
+				image[y][x][1] = 0;
+				image[y][x][2] = 0;
+			}
 		}
 	}
 
 
 
+//	for (int i = 0; i < image.cols; ++i) {
+//		for (int j = 0; j < image.rows; ++j) {
+//
+//		}
+//
+//	}
 
-	cv::imshow("olha o ronaldinho", image);
+//			double current_angle;
+//
+//			// Selecting the current angle to the next step
+//
+//			if (point.y != 0) {
+//				current_angle = atan((point.x) / (point.y));
+//			} else
+//				current_angle = M_PI_2;
+//
+//
+////				std::cout << "this is the current angle       " << minAngle / ratio << " < " << current_angle/ratio << " < " << maxAngle / ratio << std::endl;
+//
+//
+//
+//
+//			// pixels outside of sonar image
+//			if (radius >= bins || start_angle >= current_angle || current_angle >= end_angle || !radius){
+//				transfer.push_back(-1);
+//			}
+//
+//
+//			// pixels inside of sonar image
+//			else {
+//
+//					double my_bin, id_bins, id_beam;
+//					double minAngle, maxAngle;
+//					double minBin, maxBin;
+//
+//					//here ends the declaration
+//
+//
+//
+//
+//
+//
+//
+//
+//			int start_beam, end_beam;
+//			cv::Scalar color;
+//
+//
+//			if (current_angle >= 0){
+//				start_beam = beams/2.0;
+//				end_beam = 0;
+//				color = cv::Scalar(255,0,0);
+////				std::cout << "look at ronaldvinho" << std::endl;
+//
+//			} else {
+//				start_beam = beams;
+//				end_beam = beams/2.0;
+//				color = cv::Scalar(0,0,255);
+//
+////				std::cout << "------------------------------------------------------------------------------------------------" << std::endl;
+//
+//			}
+//
+//
+//
+//
+//
+//					for (int my_beam = start_beam; my_beam > end_beam; my_beam--)	{		//First bricklayer
+//
+//						image[imgy][imgx][0] = color[0];
+//						image[imgy][imgx][1] = color[1];
+//						image[imgy][imgx][2] = color[2];
+//
+//						int my_beam2 = 256 - my_beam;
+//
+//						minBin = (my_bin - 1);
+//						maxBin = my_bin;
+//
+//						minAngle =  (my_beam- 1.0) ;
+//						maxAngle = my_beam;
+//
+//
+////						std::cout << "this is the second one               " << minAngle / ratio << " < " << current_angle/ratio << " < " << maxAngle / ratio << std::endl;
+//
+//						if (current_angle >= minAngle && current_angle < maxAngle){
+//
+//
+////														std::cout << " este eh o beam -->" <<  my_beam2 << std::endl;
+////														std::cout << " este eh o raio -->" <<  int(radius) << std::endl << std::endl;
+//
+//							if(my_beam == 44) {
+//								image[imgy][imgx][0] = 255;
+//								image[imgy][imgx][1] = 255;
+//								image[imgy][imgx][2] = 255;
+//							}
+//
+////							bins_identification.push_back(radius);
+//
+////							for (int n = 0; n <= 128; ++n) {
+////
+////							std::coutdouble current_angle;
+//
+//							// Selecting the current angle to the next step
+//
+//							if (point.y != 0) {
+//								current_angle = atan((point.x) / (point.y));
+//							} else
+//								current_angle = M_PI_2;
+//
+//
+//				//				std::cout << "this is the current angle       " << minAngle / ratio << " < " << current_angle/ratio << " < " << maxAngle / ratio << std::endl;
+//
+//
+//
+//
+//							// pixels outside of sonar image
+//							if (radius >= bins || start_angle >= current_angle || current_angle >= end_angle || !radius){
+//								transfer.push_back(-1);
+//							}
+//
+//
+//							// pixels inside of sonar image
+//							else {
+//
+//									double my_bin, id_bins, id_beam;
+//									double minAngle, maxAngle;
+//									double minBin, maxBin;
+//
+//									//here ends the declaration
+//
+//
+//
+//
+//
+//
+//
+//
+//							int start_beam, end_beam;
+//							cv::Scalar color;
+//
+//
+//							if (current_angle >= 0){
+//								start_beam = beams/2.0;
+//								end_beam = 0;
+//								color = cv::Scalar(255,0,0);
+//				//				std::cout << "look at ronaldvinho" << std::endl;
+//
+//							} else {
+//								start_beam = beams;
+//								end_beam = beams/2.0;
+//								color = cv::Scalar(0,0,255);
+//
+//				//				std::cout << "------------------------------------------------------------------------------------------------" << std::endl;
+//
+//							}
+//
+//
+//
+//
+//
+//									for (int my_beam = start_beam; my_beam > end_beam; my_beam--)	{		//First bricklayer
+//
+//										image[imgy][imgx][0] = color[0];
+//										image[imgy][imgx][1] = color[1];
+//										image[imgy][imgx][2] = color[2];
+//
+//										int my_beam2 = 256 - my_beam;
+//
+//										minBin = (my_bin - 1);
+//										maxBin = my_bin;
+//
+//										minAngle =  (my_beam- 1.0) ;
+//										maxAngle = my_beam;
+//
+//
+//				//						std::cout << "this is the second one               " << minAngle / ratio << " < " << current_angle/ratio << " < " << maxAngle / ratio << std::endl;
+//
+//										if (current_angle >= minAngle && current_angle < maxAngle){
+//
+//
+//				//														std::cout << " este eh o beam -->" <<  my_beam2 << std::endl;
+//				//														std::cout << " este eh o raio -->" <<  int(radius) << std::endl << std::endl;
+//
+//											if(my_beam == 44) {
+//												image[imgy][imgx][0] = 255;
+//												image[imgy][imgx][1] = 255;
+//												image[imgy][imgx][2] = 255;
+//											}
+//
+//				//							bins_identification.push_back(radius);
+//
+//				//							for (int n = 0; n <= 128; ++n) {
+//				//
+//				//							std::cout << beams_identification[n] << std::endl;
+//				//
+//				//							}
+//
+//
+//											break;
+//										}
+//
+//
+//									}
+//
+//							}
+//
+//
+//
+//
+//							// Here ends the sweep
+//						}
+//					}
+//
+// << beams_identification[n] << std::endl;
+//
+//							}
+
+
+//							break;
+//						}
+
+
+//					}
+
+//			}
+
+
+
+
+			// Here ends the sweep
+//		}
+//	}
+
+
+
+
+
+
+	cv::imshow("Sonar View", image);
 	cv::waitKey();
 	return 0;
 }
